06b8b335d65e634e8fbad1bca3c630b2b5c2efd8
DmytroVyrishchynskyi <magic.scholz@gmail.com>
2024-12-08 10:08:55 +0200

    SeleniumHomework

--- src/main/java/com/example/selenium/pages/MainPage.java
+++ src/main/java/com/example/selenium/pages/MainPage.java
@@ -1,22 +1,27 @@
 package com.example.selenium.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
-public class MainPage extends com.example.selenium.pages.BasePage {
+public class MainPage extends BasePage {
 
-    // TODO define title and logout WebElements using @FindBy
+    @FindBy(css = "h2")
+    private WebElement title;
+
+    @FindBy(css = ".icon-2x")
+    private WebElement logoutButton;
 
     public MainPage(WebDriver driver) {
         super(driver);
     }
 
     public String getTitle() {
-        // TODO return title text
-        return null;
+        return title.getText();
     }
 
     public MainPage clickLogout() {
-        // TODO click logout and return MainPage
+        logoutButton.click();
         return this;
     }
 }
--- src/test/java/com/example/selenium/BaseTest.java
+++ src/test/java/com/example/selenium/BaseTest.java
@@ -1,7 +1,9 @@
 package com.example.selenium;
 
 import com.example.selenium.steps.UserSteps;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
@@ -11,11 +13,17 @@ public class BaseTest {
 
     @BeforeClass
     public void setUp() {
-        // TODO initialize a driver, open login URL, resize windows, and initialize steps
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/login");
+        driver.manage().window().setSize(new org.openqa.selenium.Dimension(840, 1027));
+        userSteps = new UserSteps(driver);
     }
 
     @AfterClass
     public void tearDown() {
-        // TODO driver close and quit
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
